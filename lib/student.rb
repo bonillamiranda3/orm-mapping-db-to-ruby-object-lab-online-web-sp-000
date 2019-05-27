75ebf3025add93a67ea41dbdb1ab96397d3329ec
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-27 23:08:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ class Student
     SELECT * FROM students
     WHERE grade = ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
