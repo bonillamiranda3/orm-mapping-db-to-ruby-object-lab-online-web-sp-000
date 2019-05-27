631e36a365aeada084169fd6189417bddc52c49f
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-27 23:01:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,6 +43,15 @@ class Student
       self.new_from_db(row)
     end
   end
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
   def self.students_below_12th_grade
     sql = <<-SQL
